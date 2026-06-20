45555cc747eeacf38d0cad6648d8c517b9003050
Rémi L <rem.louedec@laposte.net>
2026-06-20 09:22:18 +0200

    creation projet maven

---
+++ src/main/java/com/ciril/test/App.java
@@ -0,0 +1,13 @@
+package com.ciril.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
